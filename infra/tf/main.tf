84cabe1d26f888e86eb0a335519e7c2524fbddd4
Aliaksei SHutau <ashutau@exadel.com>
2023-03-02 22:06:24 +0400

    Add argocd

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -195,3 +195,47 @@ resource "null_resource" "download_kubeconfig_file" {
     wait_for_bootstrap_to_finish = null_resource.wait_for_bootstrap_to_finish.id
   }
 }
+
+#------------------------------------------------------------------------------#
+# Deploy ArgoCD server
+#------------------------------------------------------------------------------#
+
+provider "kubectl" {
+  config_path = "~/.kube/microk8s"
+}
+
+data "kubectl_file_documents" "namespace" {
+    content = file("../argocd/namespace.yaml")
+} 
+
+data "kubectl_file_documents" "argocd" {
+    content = file("../argocd/install.yaml")
+}
+
+resource "kubectl_manifest" "namespace" {
+    count     = length(data.kubectl_file_documents.namespace.documents)
+    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
+    override_namespace = "argocd"
+}
+
+resource "kubectl_manifest" "argocd" {
+    depends_on = [
+      kubectl_manifest.namespace,
+    ]
+    count     = length(data.kubectl_file_documents.argocd.documents)
+    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
+    override_namespace = "argocd"
+}
+
+data "kubectl_file_documents" "app" {
+    content = file("../argocd/2048-app.yaml")
+}
+
+resource "kubectl_manifest" "app" {
+    depends_on = [
+      kubectl_manifest.argocd,
+    ]
+    count     = length(data.kubectl_file_documents.app.documents)
+    yaml_body = element(data.kubectl_file_documents.app.documents, count.index)
+    override_namespace = "argocd"
+}
