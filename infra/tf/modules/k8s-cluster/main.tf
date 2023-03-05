9a5dab10bbc63f0994141aa11c4a316fd4b4d742
Aliaksei SHutau <ashutau@exadel.com>
2023-03-06 00:15:40 +0400

    generate ssh key

--- infra/tf/modules/k8s-cluster/main.tf
+++ infra/tf/modules/k8s-cluster/main.tf
@@ -1,4 +1,31 @@
 #------------------------------------------------------------------------------#
+# Generate key pair
+#------------------------------------------------------------------------------#
+variable "key_name" {
+  description = "Generated key pair name"
+  type        = string
+  default     = "k8s-key"
+}
+
+resource "tls_private_key" "terrafrom_generated_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.terrafrom_generated_private_key.public_key_openssh
+
+  # Store private key :  Generate and save private key(aws_keys_pairs.pem) in current directory
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo '${tls_private_key.terrafrom_generated_private_key.private_key_pem}' > ~/.ssh/${var.key_name}.pem
+      chmod 400 ~/.ssh/${var.key_name}.pem
+    EOT
+  }
+}
+
+#------------------------------------------------------------------------------#
 # Elastic IP for master node
 #------------------------------------------------------------------------------#
 
@@ -18,12 +45,13 @@ resource "aws_eip_association" "master" {
 #------------------------------------------------------------------------------#
 # Route53 Update Hosted zone record with eip
 #------------------------------------------------------------------------------#
-resource "aws_route53_zone" "primary" {
-  name = "avs-it.net"
-}
+# resource "aws_route53_zone" "primary" {
+#   name = "avs-it.net"
+# }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  # zone_id = aws_route53_zone.primary.zone_id
+  zone_id = "Z01980993H1VZXH19ZE7R"
   name    = "avs-it.net"
   type    = "A"
   ttl     = 300
@@ -47,7 +75,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "master" {
   ami           = data.aws_ami.ubuntu.image_id # "ami-0dfcb1ef8550277af"
   instance_type = var.master_instance_type     # "t3.small"
-  key_name      = "n_virginia_key"
+#  key_name      = "n_virginia_key"
+  key_name      = var.key_name
   subnet_id     = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   user_data_replace_on_change = true
@@ -72,7 +101,7 @@ resource "aws_instance" "master" {
 resource "null_resource" "wait_for_bootstrap_to_finish" {
   provisioner "local-exec" {
     command = <<-EOF
-    alias ssh='ssh -q -i ~/.ssh/n_virginia_key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+    alias ssh='ssh -q -i ~/.ssh/${var.key_name}.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
     while true; do
       sleep 2
       ! ssh ubuntu@${aws_eip.master.public_ip} [[ -f /home/ubuntu/microk8s ]] >/dev/null && continue
@@ -92,7 +121,7 @@ resource "null_resource" "wait_for_bootstrap_to_finish" {
 resource "null_resource" "download_kubeconfig_file" {
   provisioner "local-exec" {
     command = <<-EOF
-    alias microk8s_scp='scp -q -i ~/.ssh/n_virginia_key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+    alias microk8s_scp='scp -q -i ~/.ssh/${var.key_name}.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
     microk8s_scp ubuntu@${aws_eip.master.public_ip}:~/microk8s ~/.kube/microk8s
     sed -i "s/server:.*/server: https:\/\/${aws_eip.master.public_ip}:16443/g" ~/.kube/microk8s
     EOF
